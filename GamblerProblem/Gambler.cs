909c92e12b9a3c25a0cbc5865945059bf9485a89
Akash-Patil-12 <patilakash121212@gmail.com>
2021-08-18 09:35:41 +0530

    [Akash] Add : Implemented Start Gambler Problem stack with 100 doller every day

---
+++ GamblerProblem/Gambler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblerProblem
+{
+    class Gambler
+    {
+        public void Bet()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(100);
+            Random random = new Random();
+            int player = random.Next(0,2);
+            Console.WriteLine(player);
+            if (player == 0)
+            {
+                Console.WriteLine("Loss game");
+                int stackPreviousValue = stack.Pop();
+                stack.Push(stackPreviousValue - 1);
+            }
+            else
+            {
+                Console.WriteLine("Won the game");
+                int stackPreviousValue = stack.Pop();
+                stack.Push(stackPreviousValue + 1);
+            }
+            Console.WriteLine("After Game play Stack Value is =" + stack.Peek());
+        }
+    }
+}
---
+++ GamblerProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GamblerProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gambler gambler = new Gambler();
+            gambler.Bet();
+        }
+    }
+}
